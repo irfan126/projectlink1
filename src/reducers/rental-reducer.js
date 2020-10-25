c37c743b3f1cab6547c0d81a618a451d458f9ffa
irfan <irfan126@gmail.com>
2020-10-25 19:19:03 +0000

    Adds rental detail and Rental listing page. Adds bootstrap and scss. Adds redux architecture and routing

---
+++ src/reducers/rental-reducer.js
@@ -0,0 +1,40 @@
+import { FETCH_RENTALS, 
+        FETCH_RENTAL_BY_ID_SUCCESS,
+        FETCH_RENTAL_BY_ID_INIT } from '../actions/types';
+
+const INITIAL_STATE = {
+  rentals: {
+    data:[]
+  },
+  rental: {
+    data: {}
+  }
+}
+
+export const rentalReducer = (state = INITIAL_STATE.rentals, action) => {
+  switch(action.type) {
+    case FETCH_RENTALS:
+      return {...state, data: action.rentals}
+  //  case FETCH_RENTALS_INIT:
+  //    return {...state, data: [], errors: []};
+  //  case FETCH_RENTALS_SUCCESS:
+  //    return {...state, data: action.rentals};
+  //  case FETCH_RENTALS_FAIL:
+  //    return Object.assign({}, state, {errors: action.errors, data: []});
+    default:
+      return state;
+  }
+}
+
+export const selectedRentalReducer = (state = INITIAL_STATE.rental, action) => {
+  switch(action.type) {
+    case FETCH_RENTAL_BY_ID_INIT:
+      return {...state, data: {}};
+    case FETCH_RENTAL_BY_ID_SUCCESS:
+    //return Object.assign({}, state, { data: action.rental});
+      return {...state, data: action.rental}
+
+    default:
+      return state;
+  }
+}
